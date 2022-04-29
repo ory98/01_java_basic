25fe69e4cc18acdc1d333906e81863ec7d280d1a
HI JIN <HI JIN@192.168.123.104>
2022-04-29 12:02:02 +0900

    4/29 집공부

--- 01_java_basic/src/step1_05/controlStatement/ifEx28_테스트문제.java
+++ 01_java_basic/src/step1_05/controlStatement/ifEx28_테스트문제.java
@@ -26,14 +26,40 @@ public class ifEx28_테스트문제 {
 		Scanner scan = new Scanner(System.in);
 		Random ran = new Random();
 		
+		System.out.print("가위(0)바위(1)보(2) 나의 왼쪽 입력 :");
 		int meLeft = scan.nextInt();
 		
+		System.out.print("가위(0)바위(1)보(2) 나의 오른쪽 입력 :");
+		int meRight = scan.nextInt();
+		
+		System.out.print("둘 중 하나 입력(왼쪽 : 3 / 오른쪽 : 4) :");
+		int meFinal = scan.nextInt();
+		
+		if (meFinal == 3) meFinal = meLeft;
+		if (meFinal == 4) meFinal = meRight;
+		
+
+		int comLeft = ran.nextInt(3);
+		int comRight = ran.nextInt(3);
+		
+		int comFinal = ran.nextInt(2);
+		
+		if (comFinal == 0) comFinal = comLeft;
+		if (comFinal == 1) comFinal = comRight;		
 		
 		
+		if (comFinal == meFinal) System.out.println("\n비김");
 		
+		else if (meFinal == 0 && comFinal == 2 || meFinal == 1 && comFinal == 0 || meFinal == 2 && comFinal == 1) {
+			System.out.println("\n이겼다");
+		}
 		
+		else System.out.println("\n졌다");
+	
 		
+		System.out.println("\n나의 답 : " + meFinal + " vs 컴퓨터의 답 : " + comFinal);
 		
+		scan.close();
 		
 		
 	}
